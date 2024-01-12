6184a179c063d40424458673ac0033ee2e04a12c
99894 <developerbola08@gmail.com>
2024-01-12 19:49:16 +0500

    INFRA:Initialize Api Project

---
+++ VisionCraft/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VisionCraft.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async ValueTask<ActionResult<string>> Get() =>
+            Ok("Okkey");
+    }
+}
