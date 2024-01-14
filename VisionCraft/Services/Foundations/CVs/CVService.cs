df0795dce8bbd20d7d08afe4c45959f4ba7080ef
Golibjon <tuxtamatovgolibjon@gmail.com>
2024-01-14 10:32:44 +0500

    FOUNDATIONS: AddCV WIP

---
+++ VisionCraft/Services/Foundations/CVs/CVService.cs
@@ -0,0 +1,19 @@
+﻿using VisionCraft.Brokers.Storages;
+using VisionCraft.Models.CVs;
+
+namespace VisionCraft.Services.Foundations.CVs
+{
+    public class CVService : ICVService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public CVService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public ValueTask<CV> AddCVAsync(CV cv) =>
+            this.storageBroker.InsertCVAsync(cv);
+
+    }
+}
