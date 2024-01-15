eef8f4bd5365281c3d9d53c39399cf655c9df336
Golibjon <tuxtamatovgolibjon@gmail.com>
2024-01-15 16:48:32 +0500

    WIP

---
+++ VisionCraft/Services/Foundations/Pdfs/IPdfService.cs
@@ -0,0 +1,7 @@
+﻿namespace VisionCraft.Services.Foundations.Pdfs
+{
+    public interface IPdfService
+    {
+        ValueTask<string> ReadExtracPdfAsync(Stream pdfFile);
+    }
+}
---
+++ VisionCraft/Services/Foundations/Pdfs/PdfService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace VisionCraft.Services.Foundations.Pdfs
+{
+    public class PdfService : IPdfService
+    {
+        public ValueTask<string> ReadExtracPdfAsync(Stream pdfFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
