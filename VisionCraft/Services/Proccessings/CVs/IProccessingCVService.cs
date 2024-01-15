1a9f13ffd539fb8ce595c04686fac8ca5c2febc7
Golibjon <tuxtamatovgolibjon@gmail.com>
2024-01-15 15:44:03 +0500

    POC: CV Services

---
+++ VisionCraft/Services/Proccessings/CVs/IProccessingCVService.cs
@@ -0,0 +1,12 @@
+﻿using VisionCraft.Models.CVs;
+
+namespace VisionCraft.Services.Proccessings.CVs
+{
+    public interface IProccessingCVService
+    {
+        ValueTask<CV> ProcAddCVAsync(CV cv);
+        IQueryable<CV> ProcRetrieveAllCVs();
+        ValueTask<CV> ProcRetrieveCvByIdAsync(Guid id);
+        ValueTask<CV> ProcRemoveCVAsync(Guid id);
+    }
+}
---
+++ VisionCraft/Services/Proccessings/CVs/ProccessingCVService.cs
@@ -0,0 +1,27 @@
+﻿using VisionCraft.Models.CVs;
+using VisionCraft.Services.Foundations.CVs;
+
+namespace VisionCraft.Services.Proccessings.CVs
+{
+    public class ProccessingCVService : IProccessingCVService
+    {
+        private readonly ICVService cVService;
+
+        public ProccessingCVService(ICVService cVService)
+        {
+            this.cVService = cVService;
+        }
+
+        public async ValueTask<CV> ProcAddCVAsync(CV cv) =>
+            await this.cVService.AddCVAsync(cv);
+
+        public IQueryable<CV> ProcRetrieveAllCVs() =>
+            this.cVService.RetrieveAllCVs();
+
+        public async ValueTask<CV> ProcRetrieveCvByIdAsync(Guid id) =>
+            await this.cVService.RetrieveCvByIdAsync(id);
+
+        public async ValueTask<CV> ProcRemoveCVAsync(Guid id) =>
+            await this.cVService.RemoveCVAsync(id);
+    }
+}
