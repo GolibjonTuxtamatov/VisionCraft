d602a63268ce3b45ca558981f618ce6ebff14e15
Golibjon <tuxtamatovgolibjon@gmail.com>
2024-01-15 09:13:33 +0500

    ShouldThrowValidationExceptionOnAddIfCVIsInvalidAndLogItAsync -> FAIL

--- VisionCraft.Tests.Unit/Services/Foundations/CVs/CVServiceTests.Validations.Add.cs
+++ VisionCraft.Tests.Unit/Services/Foundations/CVs/CVServiceTests.Validations.Add.cs
@@ -45,5 +45,41 @@ namespace VisionCraft.Tests.Unit.Services.Foundations.CVs
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfCVIsInvalidAndLogItAsync()
+        {
+            //given
+            var invalidCV = new CV();
+
+            var invalidCVException = new InvalidCVException();
+
+            invalidCVException.AddData(
+                key: nameof(CV.Id),
+                values: "Id id required");
+
+            var expectedCVValidationException =
+                new CVValidationException(invalidCVException);
+
+            //when
+            ValueTask<CV> addCVTask = this.cVService.AddCVAsync(invalidCV);
+
+            CVValidationException actualCVValidationException =
+                await Assert.ThrowsAsync<CVValidationException>(addCVTask.AsTask);
+
+            //then
+            actualCVValidationException.Should().BeEquivalentTo(expectedCVValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCVValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCVAsync(It.IsAny<CV>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
