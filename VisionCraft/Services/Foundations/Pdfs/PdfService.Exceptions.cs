099140388b2cde28a78c336203de2f51dab5d872
Golibjon <tuxtamatovgolibjon@gmail.com>
2024-01-26 19:42:27 +0500

    Checked PdfStream for null

---
+++ VisionCraft/Services/Foundations/Pdfs/PdfService.Exceptions.cs
@@ -0,0 +1,30 @@
+﻿using VisionCraft.Models.Pdf.Exceptions;
+using Xeptions;
+
+namespace VisionCraft.Services.Foundations.Pdfs
+{
+    public partial class PdfService
+    {
+        private delegate ValueTask<string> UploadPdfFunction();
+
+        private ValueTask<string> TryCatch(UploadPdfFunction uploadPdfFunction)
+        {
+            try
+            {
+                return uploadPdfFunction();
+            }
+            catch (NullPdfException nullpdfException)
+            {
+                throw CreateAndLogValidationException(nullpdfException);
+            }
+        }
+
+        private PdfValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var pdfValidationException = new PdfValidationException(exception);
+            this.loggingBroker.LogError(pdfValidationException);
+
+            return pdfValidationException;
+        }
+    }
+}
---
+++ VisionCraft/Services/Foundations/Pdfs/PdfService.Validations.cs
@@ -0,0 +1,13 @@
+﻿using VisionCraft.Models.Pdf.Exceptions;
+
+namespace VisionCraft.Services.Foundations.Pdfs
+{
+    public partial class PdfService
+    {
+        private static void ValidatePdfNotNull(Stream pdf)
+        {
+            if (pdf is null)
+                throw new NullPdfException();
+        }
+    }
+}
